430274cf645d4a6156a84130134cf4375cc78bb8
anano303 <124179773+anano303@users.noreply.github.com>
2025-07-11 23:43:55 +0400

    პერფორმანსის სიჩქარის გასწორება

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "../styles/performance.css";
 import { Providers } from "./providers";
 // import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from "@/components/auth-provider";
